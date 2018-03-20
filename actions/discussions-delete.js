e3a29c69a7039030db1d2cfd7f145e7b47c72bc8
Seth Childers <sethchilders92@gmail.com>
2018-03-20 13:30:53 -0600

    Small changes

--- actions/discussions-delete.js
+++ actions/discussions-delete.js
@@ -17,14 +17,23 @@ module.exports = (course, discussion, callback) => {
 
     /* This is the action that happens if the test is passed */
     function action() {
-        discussion.techops.delete = true;
-        discussion.techops.log('Discussions Deleted', {
+        var logCategory = 'Discussion - Deleted';
+
+        /* If we're running a standards check and not doing any changes... */
+        if (course.info.checkStandard === true) {
+            logCategory = 'Discussion - Deprecated';
+        } else {
+            discussion.techops.delete = true;
+        }
+
+        discussion.techops.log(logCategory, {
             'Title': discussion.title,
             'ID': discussion.id
         });
         callback(null, course, discussion);
     }
 
+    /* The Test */
     if (found != undefined) {
         action();
     } else {
