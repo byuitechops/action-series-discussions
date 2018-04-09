2358e9cac084481129821da611ac961fd135c938
Danverde <gre12014@byui.edu>
2018-04-09 13:14:10 -0600

    set default platforms

--- actions/discussions-delete.js
+++ actions/discussions-delete.js
@@ -1,6 +1,6 @@
 module.exports = (course, discussion, callback) => {
     //only add the platforms your grandchild should run in
-    var validPlatforms = ['online', 'pathway', 'campus'];
+    var validPlatforms = ['online', 'pathway'];
     var validPlatform = validPlatforms.includes(course.settings.platform);
 
     /* If the item is marked for deletion, do nothing */
